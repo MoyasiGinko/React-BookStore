909efd8d7baabee3ff74607c93597aa43bb88688
Moyasi Ginko <mrsiyam61@gmail.com>
2023-06-24 17:50:25 +0600

    Add initialstate variable

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,8 +1,29 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const initialState = [
+  {
+    id: '1',
+    title: 'The Great Gatsby',
+    author: 'John Smith',
+    category: 'Fiction',
+  },
+  {
+    id: '2',
+    title: 'Anna Karenina',
+    author: 'Leo Tolstoy',
+    category: 'Fiction',
+  },
+  {
+    id: '3',
+    title: 'The Selfish Gene',
+    author: 'Richard Dawkins',
+    category: 'Nonfiction',
+  },
+];
+
 const booksSlice = createSlice({
   name: 'books',
-  initialState: [],
+  initialState,
   reducers: {
     addBook: (state, action) => {
       state.push(action.payload);
