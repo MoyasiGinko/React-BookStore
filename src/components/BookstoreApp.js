302e1c41605b8af4348454a6dc0db1b8d2e1da01
Moyasi Ginko <mrsiyam61@gmail.com>
2023-06-24 22:20:56 +0600

    Implement redux

--- src/components/BookstoreApp.js
+++ src/components/BookstoreApp.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import BookList from './booksList';
 import AddBook from './addBooks';
-import { addBook, removeBook } from '../redux/books/booksSlice';
+import { fetchBooks, addBook, removeBook } from '../redux/books/asyncThunks';
 
 const Store = () => {
   const books = useSelector((state) => state.books);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
 
   const handleAddBook = (book) => {
     dispatch(addBook(book));
