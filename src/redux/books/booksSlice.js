cb8393cbfea49c80be74a8d02c12c89f7c46783e
Moyasi Ginko <mrsiyam61@gmail.com>
2023-06-24 22:22:40 +0600

    Update booksSlice and separate the functions into asyncThunk.js file

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,46 +1,5 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
-
-const API_BASE_URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi';
-const KEY = 'eWWpOQYFfW4okklL3oVZ';
-const FETCH_BOOKS = 'books/fetchBooks';
-const ADD_BOOKS = 'books/addBook';
-const REMOVE_BOOK = 'books/removeBook';
-
-export const fetchBooks = createAsyncThunk(FETCH_BOOKS, async () => {
-  const res = await axios.get(`${API_BASE_URL}/apps/${KEY}/books`);
-  const booksArray = Object.entries(res.data).map(([id, info]) => ({
-    id,
-    ...info[0],
-  }));
-
-  return booksArray;
-});
-
-export const addBook = createAsyncThunk(ADD_BOOKS, async (bookData) => {
-  const bookId = Date.now();
-
-  const { title, author, category } = bookData;
-
-  await axios.post(`${API_BASE_URL}/apps/${KEY}/books`, {
-    item_id: bookId,
-    title,
-    author,
-    category: category || '',
-  });
-
-  return {
-    item_id: bookId,
-    title,
-    author,
-    category: category || '',
-  };
-});
-
-export const removeBook = createAsyncThunk(REMOVE_BOOK, async (bookId) => {
-  await axios.delete(`${API_BASE_URL}/apps/${KEY}/books/${bookId}`);
-  return bookId;
-});
+import { createSlice } from '@reduxjs/toolkit';
+import { fetchBooks, addBook, removeBook } from './asyncThunks';
 
 const initialState = [];
 
@@ -54,15 +13,9 @@ const booksSlice = createSlice({
         state.length = 0;
         action.payload.forEach((book) => state.push(book));
       })
-      .addCase(addBook.fulfilled, (state, action) => {
-        state.push(action.payload);
-      })
-      .addCase(removeBook.fulfilled, (state, action) => {
-        const index = state.findIndex((book) => book.id === action.payload);
-        if (index !== -1) {
-          state.splice(index, 1);
-        }
-      });
+      .addCase(addBook.fulfilled, (state, action) => [...state, action.payload])
+      .addCase(removeBook.fulfilled,
+        (state, action) => state.filter((book) => book.id !== action.payload));
   },
 });
 
